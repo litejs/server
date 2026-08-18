a6950acb8927d59b5d775bf674cfc517ba39ef56
Lauri Rooden <lauri@rooden.ee>
2026-08-18 13:06:18 +0300

    Migrate linting to ESLint

---
+++ .github/eslint.config.mjs
@@ -0,0 +1,45 @@
+export default [{
+	files: ['.github/eslint.config.mjs', '*.mjs', 'lib/*.mjs'],
+	languageOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		globals: {
+			addEventListener: 'readonly',
+			atob: 'readonly',
+			btoa: 'readonly',
+			Bun: 'readonly',
+			caches: 'readonly',
+			clearTimeout: 'readonly',
+			clients: 'readonly',
+			console: 'readonly',
+			crypto: 'readonly',
+			Deno: 'readonly',
+			fetch: 'readonly',
+			Headers: 'readonly',
+			process: 'readonly',
+			ReadableStream: 'readonly',
+			Request: 'readonly',
+			Response: 'readonly',
+			setInterval: 'readonly',
+			setTimeout: 'readonly',
+			skipWaiting: 'readonly',
+			TextDecoder: 'readonly',
+			TextEncoder: 'readonly',
+			tjs: 'readonly',
+			URL: 'readonly',
+			WebSocketPair: 'readonly'
+		}
+	},
+	linterOptions: {
+		reportUnusedDisableDirectives: 'error'
+	},
+	rules: {
+		'max-depth': ['error', 6],
+		'no-irregular-whitespace': 'error',
+		'no-new': 'error',
+		'no-undef': 'error',
+		'no-unused-vars': ['error', { args: 'after-used', caughtErrors: 'none' }],
+		quotes: ['error', 'single', { avoidEscape: true }],
+		semi: ['error', 'never']
+	}
+}]
